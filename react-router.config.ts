e6ea54302b0229052fdb4ea61d2b63299406d65c
Grzegorz Kniażuk <grzegorz.kniazuk@gmail.com>
2025-11-11 22:53:35 +0100

    set react router basename

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,6 @@ import type { Config } from "@react-router/dev/config";
 export default {
   appDirectory: "app",
   buildDirectory: "dist",
+  basename: '/speakout-quiz',
   ssr: false,
 } satisfies Config;
